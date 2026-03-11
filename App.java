1782cd8d289f01c568f60afc6c0b7225fc7175c0
Desarrollador Java Script <jhonson123456789castro@gmail.com>
2026-03-10 20:47:04 -0500

    Add restar method to perform subtraction

--- App.java
+++ App.java
@@ -1,7 +1,7 @@
 public class App {
 
-    public static double suma(double a, double b) {
-        return a + b;
+    public static double restar(double a, double b) {
+        return a - b;
     }
 
 }
