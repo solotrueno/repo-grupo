a245cb568ea0a14e4781e4bb51ca6d9aa7fc14da
3daniela <danielacolorado2134@gmail.com>
2026-03-10 19:46:19 -0500

    tercer commit

--- App.java
+++ App.java
@@ -4,4 +4,17 @@ public class App {
         return a + b;
     }
 
-}
+     public static double resta(double a, double b) {
+        return a - b;
+    }
+    
+     public static double multiplicacion(double a, double b) {
+        return a * b;
+    }
+
+     public static double division(double a, double b) {
+        if (b == 0) {
+            throw new IllegalArgumentException("No se puede dividir por cero");
+        }
+        return a / b;
+    }
